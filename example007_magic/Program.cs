c5cf0d4adddb242b1090036f9ffc5f3ad383e5ba
Name <limpamposha@mail.ru>
2022-11-24 16:45:43 +0300

    example007

--- example007_magic/Program.cs
+++ example007_magic/Program.cs
@@ -4,13 +4,13 @@
 int xa = 40, ya = 1, 
     xb = 1, yb = 30,
     xc = 80, yc = 30;
-
+  
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine"");
+Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine"");
+Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine"");
+Console.WriteLine("+");
 
 int x = xa, y = xb;
 
